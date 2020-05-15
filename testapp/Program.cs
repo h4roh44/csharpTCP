2eadbe24fdfd0e430a66a71efeb7588f8dc629f7
mastersplint4 <mastersplint4@gmail.com>
2020-05-14 21:36:37 -0500

    Initial Commit of Server and Client

---
+++ testapp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace testapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress IP = IPAddress.Parse("127.0.0.1");
+             // IP Server will run on
+
+            TcpListener tcplist = new TcpListener(IP,6969);
+            // Initialize TCP Listener on server
+
+            tcplist.Start();
+            // Start the TCP Listener
+
+            Console.WriteLine("Starting TCP Listener on " + IP + ":6969");
+            Console.WriteLine("Be patient, I'll accept your socket in one second");
+
+            Socket sock = tcplist.AcceptSocket();
+            Console.WriteLine("See what did I say? I accepted your connection from " + sock.RemoteEndPoint);
+
+
+
+        }
+
+    }
+}
---
+++ testappclnt/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+namespace testappclnt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string server = "localhost";
+            int port = 6969;
+            // declare Server variables
+
+            TcpClient tcpclnt = new TcpClient();
+            // Create TCP Client
+
+            Console.WriteLine("Connecting");
+
+            tcpclnt.Connect(server, port);
+            // Connect using the TCP Client
+
+            if (tcpclnt.Connected == true)
+            {
+                Console.WriteLine("connected to " + server);
+            }
+        }
+    }
+}
